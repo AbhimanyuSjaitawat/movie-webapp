7e35b7a4bce0a850b103883b01e20de6cdd76023
Abhimanyu Singh Jaitawat <abhi1994jaitawat@gmail.com>
2017-09-28 13:17:33 +0530

    Project Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { MovieCardComponent } from './movie/movie-card/movie-card.component';
+import { MovieDetailComponent } from './movie/movie-detail/movie-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginComponent,
+    RegisterComponent,
+    MovieCardComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
